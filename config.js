6b991bf82c2dcf93714fd24e3c3a1ec7be148472
websupergirl <nikole@thatsupergirl.com>
2020-03-01 22:05:49 -0800

    fix server connection

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
   port: 8080,
   db: {
-    prod: process.env.DATABASE_URL || 'mongodb+srv://churchill-iuvo:d8h2EHR7%5Ecv%23nGt@churchill-3shc2.mongodb.net/test?retryWrites=true&w=majority',
-    test: 'mongodb+srv://churchill-iuvo:d8h2EHR7%5Ecv%23nGt@churchill-3shc2.mongodb.net/test?retryWrites=true&w=majority',
+    prod: process.env.DATABASE_URL,
     options: {
       useNewUrlParser: true,
       useCreateIndex: true,
